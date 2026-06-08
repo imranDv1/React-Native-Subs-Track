513fa9c6ebea0e5a1c7969d55a8cf460ce64a919
imran <uffmink@gmail.com>
2026-06-08 22:54:52 +0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,28 @@
 import { Link } from "expo-router";
-import {styled} from 'nativewind'
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-const SafeAreaView = styled(RNSafeAreaView)
+const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/(auth)/sign-in" className="mt-4 text-lg bg-primary text-white p-2">
+      <Text className="text-7xl font-sans-extrabold ">Home</Text>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 text-lg  font-sans-bold bg-primary text-white p-2"
+      >
         Sign In
       </Link>
-      <Link href='/subscriptions/spotify' className="mt-4 text-lg text-white bg-primary p-2">
+      <Link
+        href="/subscriptions/spotify"
+        className="mt-4 text-lg text-white font-sans-bold bg-primary p-2"
+      >
         View Spotify Subscription
       </Link>
-      <Link href={{pathname: '/subscriptions/[id]', params: {id: 'claude'}}} className="mt-4 text-lg bg-primary text-white p-2">
+      <Link
+        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
+        className="mt-4 text-lg bg-primary font-sans-bold text-white p-2"
+      >
         View Claude Subscription
       </Link>
     </SafeAreaView>
